f413feb60777fe54c694be2850544cd7e1a397e8
Kaoru <shiinasama2001@gmail.com>
2023-12-30 01:15:56 +0800

    build: project setup

---
+++ test/AllTestInMain.cpp
@@ -0,0 +1,9 @@
+#include <sese/util/Initializer.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    sese::initCore(argc, argv);
+    return RUN_ALL_TESTS();
+}
